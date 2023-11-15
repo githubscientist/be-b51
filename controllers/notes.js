82493e3f90723eba03dedcd02187e3e712483a8a
githubscientist <sathish.datascientist@gmail.com>
2023-11-15 21:25:11 +0530

    backend ready + cleaned

--- controllers/notes.js
+++ controllers/notes.js
@@ -19,4 +19,71 @@ notesRouter.post('/', (request, response) => {
         });
 });
 
+// endpoint to fetch a single resource based on it
+notesRouter.get('/:id', (request, response) => {
+    const id = request.params.id;
+
+    Note.findById(id)
+        .then(note => {
+            response.status(200).json(note);
+        })
+        .catch(err => {
+            response.status(404).json({ message: 'id does not exists' });
+        })
+});
+
+// deletes a single resource based on id
+notesRouter.delete('/:id', (request, response) => {
+    const id = request.params.id;
+
+    Note.findByIdAndDelete(id)
+        .then(deletedNote => {
+            if (deletedNote) {
+                response.status(204).json({ message: 'note deleted successfully' });
+            } else {
+                response.status(404).json({ message: 'id does not exists' });
+            }
+        })
+        .catch(err => {
+            response.status(404).json({ message: 'deleting note failed' });
+        })
+});
+
+// replaces the entire note object identified by an id
+notesRouter.put('/:id', (request, response) => {
+    const id = request.params.id;
+
+    const noteToReplace = request.body;
+
+    Note.findByIdAndUpdate(id, noteToReplace)
+        .then(updatedNote => {
+            if (updatedNote) {
+                response.status(200).json({ message: 'note replaced successfully' });
+            } else {
+                response.status(404).json({ message: 'id does not exists' });
+            }
+        })
+        .catch(err => {
+            response.status(404).json({ message: 'replacing the note failed...' });
+        })
+});
+
+notesRouter.patch('/:id', (request, response) => {
+    const id = request.params.id;
+
+    const noteToPatch = request.body;
+
+    Note.findByIdAndUpdate(id, noteToPatch)
+        .then(updatedNote => {
+            if (updatedNote) {
+                response.status(200).json({ message: 'note patched successfully' });
+            } else {
+                response.status(404).json({ message: 'id does not exists' });
+            }
+        })
+        .catch(err => {
+            response.status(404).json({ message: 'patching the note failed...' });
+        })
+});
+
 module.exports = notesRouter;
