099e98e2e8542a208cf035d697af286dc3b20926
githubscientist <sathish.datascientist@gmail.com>
2023-11-16 18:59:46 +0530

    moving db connecting code to index.js

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const app = require('./server');
 const config = require('./utils/config');
 const logger = require('./utils/logger');
+const mongoose = require('mongoose');
 
-app.listen(config.PORT, () => {
-    logger.info(`Server running on port ${config.PORT}`);
-});
+mongoose.set('strictQuery', false);
+
+logger.info('Connecting to', config.MONGODB_URI);
+mongoose.connect(config.MONGODB_URI)
+    .then(() => {
+        logger.info('Connected to MongoDB...');
+        app.listen(config.PORT, () => {
+            logger.info(`Server running on port ${config.PORT}`);
+        });
+    })
+    .catch((err) => {
+        logger.error('Error connecting to MongoDB:', err);
+    });
--- server.js
+++ server.js
@@ -1,21 +1,7 @@
 const express = require('express');
 const app = express();
-const mongoose = require('mongoose');
-const config = require('./utils/config');
-const logger = require('./utils/logger');
 const cors = require('cors');
 const notesRouter = require('./controllers/notes');
-
-mongoose.set('strictQuery', false);
-
-logger.info('Connecting to', config.MONGODB_URI);
-mongoose.connect(config.MONGODB_URI)
-    .then(() => {
-        logger.info('Connected to MongoDB...');
-    })
-    .catch((err) => {
-        logger.error('Error connecting to MongoDB:', err);
-    });
 
 app.use(cors());
 app.use(express.json());
