ee9d84f6f3454963aafdbb57f2fe8906bb001cb6
githubscientist <sathish.datascientist@gmail.com>
2023-11-16 19:50:20 +0530

    task added for day4

---
+++ task.js
@@ -0,0 +1,15 @@
+/*
+    1. Create a collection named "users" in mongodb
+    2. Create a document with the following fields:
+        - name
+        - email
+        - password
+        - createdAt
+        - updatedAt
+    3. tasks:
+        - create a model named "User" for the collection "users"
+        - create a route named "/users" in the server.js
+        - create a controller named "users.js" in the controllers folder
+        - create a router named userRouter in the users.js
+        - define the CRUD operations in the userRouter
+*/
